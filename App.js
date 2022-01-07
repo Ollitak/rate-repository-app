490684ec5639b569683934dfdceed5c2a8d19993
Olli Taka-Anttila <olli.takaanttila@gmail.com>
2022-01-07 11:43:17 +0200

    implement access token storing

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
+import AuthStorage from './src/utils/authStorage';
+import AuthStorageContext from './src/contexts/AuthStorageContext';
 
-const apolloClient = createApolloClient();
-
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 
 const App = () => {
@@ -15,7 +17,9 @@ const App = () => {
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
+          <AuthStorageContext.Provider value={authStorage}>
         <Main />
+          </AuthStorageContext.Provider>
         </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,11 +1,28 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useApolloClient } from "@apollo/client";
 import { AUTHORIZE } from "../graphql/mutations";
+import useAuthStorage from '../hooks/useAuthStorage';
+import { useHistory } from "react-router-native";
+
+
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHORIZE);
+  const authStorage = useAuthStorage();
+  let history = useHistory();
+  const apolloClient = useApolloClient();
 
   const signIn = async ({ username, password }) => {
     const response = await mutate({ variables: { data: { username, password } } });
+    
+    const { data } = response;
+
+    if(data && data.authorize) {
+      console.log("Storing access token: " + data.authorize.accessToken);
+      await authStorage.setAccessToken(data.authorize.accessToken);
+      history.push("/");
+      await apolloClient.resetStore();
+    }
+
     return response;
   };
 
